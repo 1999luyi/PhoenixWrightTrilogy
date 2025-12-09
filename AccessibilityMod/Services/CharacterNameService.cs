8b9d94184420b83e67a790eda27875989a3b047d
LordLuceus <lord.luceus@gmail.com>
2025-12-09 18:41:24 +0100

    Additional GS2 name mappings

--- AccessibilityMod/Services/CharacterNameService.cs
+++ AccessibilityMod/Services/CharacterNameService.cs
@@ -101,6 +101,9 @@ namespace AccessibilityMod.Services
             { 46, "Russell Berry" },
             { 47, "Bellboy" },
             { 48, "PA Notice" },
+            { 52, "Guard" },
+            { 53, "Shoe" },
+            { 54, "John Doe" },
         };
 
         // GS3 speaker IDs - must be filled in by playing the game
